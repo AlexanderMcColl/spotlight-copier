1b3de9e09be040576dbbaa8f56e15e3a1aab9b56
Alexander McColl <34637358+AlexanderMcColl@users.noreply.github.com>
2023-05-09 15:53:12 +0100

    added comments, split code into 3 loops

--- ScheduledTaskCreator.ps1
+++ ScheduledTaskCreator.ps1
@@ -1,21 +1,16 @@
-<#
-Add-Type -AssemblyName System.Windows.Forms
-$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
-    InitialDirectory = [Environment]::GetFolderPath('Desktop')
-    Filter           = '*.ps1'
-}
-$Null = $FileBrowser.ShowDialog()
+# As long as you've stored both .ps1 files in the same folder, this automatically fetches the correct path to create the Schedueld Task
+$thisFilePath = Split-Path $MyInvocation.MyCommand.Path -Parent
+$scriptFilePath = $thisFilePath + "\SpotlightCopier.ps1"
+$scriptFilePathQuoted = """$($scriptFilePath)"""
 
-Write-Host $FileBrowser.FileName.Value
-#>
-
-$scriptFilePath = '"D:\Dev\Windows wallpaper copier (PS)\SpotlightCopier.ps1"'
+# Set all the variables needed for Windows Scheduled Task creation
 $userid = (Get-CimInstance -ClassName Win32_ComputerSystem).Username
-
-$description = "Copy images out of the Windows Spotlight folder after logon after computer has been idle for 5 seconds"
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-file $($scriptFilePath)"
+$description = "Copy images out of the Windows Spotlight folder at logon"
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-file $($scriptFilePathQuoted)"
 $trigger = New-ScheduledTaskTrigger -AtLogOn -User $userid
 $principal = New-ScheduledTaskPrincipal -UserId $userid -LogonType Interactive
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopOnIdleEnd -DontStopIfGoingOnBatteries -ExecutionTimeLimit (New-TimeSpan -Hours 1)
 $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings -Description $description
+
+# Create the task
 Register-ScheduledTask "Copy Windows Spotlight Images" -InputObject $task
--- SpotlightCopier.ps1
+++ SpotlightCopier.ps1
@@ -1,3 +1,5 @@
+# You'll need to set your own $workingFolder.
+# If your Landscape and Portrait folders aren't subfolders of your working folder, you'll need to customise those too.
 $spotlightFolder = "$($env:LOCALAPPDATA)\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\"
 $workingFolder = "D:\Pictures\Wallpapers\Spotlight Wallpapers\"
 $landscapeFolder = $workingFolder + "Landscape\"
@@ -5,7 +7,8 @@ $portraitFolder = $workingFolder + "Portrait\"
 $ext = ".jpg"
 $copiedFiles = @{}
 
-
+# Copy all files out of the Spotlight folder that are greater than 200KB.
+# Record the filename as a hashtable key, and record the width and height of the image as values associated with that key.
 foreach ($file in Get-ChildItem -Path $spotlightFolder) {
     if ($file.length -gt 200KB) {
         $imageFile = $spotlightFolder + $file
@@ -20,24 +23,40 @@ foreach ($file in Get-ChildItem -Path $spotlightFolder) {
     }
 }
 
+# Give the copied images .jpg extensions
 foreach ($h in $copiedFiles.GetEnumerator() ) {
     $image = $workingFolder + $h.Name
     $imageExt = $image + $ext
-    if (-not(Test-Path $imageExt)) {
-        if (Test-Path $image) {
-            Rename-Item $image $imageExt
-            $landscapeCheck = $landscapeFolder + $h.Name + $ext
-            $portraitCheck = $PortraitFolder + $h.Name + $ext
-            if (-not(Test-Path $landscapeCheck) -and -not(Test-Path $portraitCheck)) {          
-                if ($h.Value[0] -gt $h.Value[1]) {
-                    Move-Item $imageExt $landscapeFolder
-                }
-                elseif ($h.Value[1] -gt $h.Value[0]) {
-                    Move-Item $imageExt $portraitFolder
-                }
-            }
-            else { Remove-Item $imageExt }
+    # As long as there is an image in the folder with this filename, proceed.
+    # Else remove it from the hashtable.
+    if (Test-Path $image) {
+        # If there's already an image.jpg file in this folder, delete this image file and remove it from the hashtable.
+        # Else as long as this file is in the folder (was copied successfully) append .jpg onto the filename (give it a jpg extension).
+        if (Test-Path $imageExt) {
+            Remove-Item $image
+            $copiedFiles.Remove($h)
+        }
+        else { Rename-Item $image $imageExt }
+    }
+    else { $copiedFiles.Remove($h) }
+}
+
+# Move the jpg images into folders for landscape and portrait.
+foreach ($h in $copiedFiles.GetEnumerator() ) {
+    $imageExt = $workingFolder + $h.Name + $ext
+    $landscapeCheck = $landscapeFolder + $h.Name + $ext
+    $portraitCheck = $PortraitFolder + $h.Name + $ext
+    # Check the Portait and Landscape folders for this image.jpg. As long as it isn't in either folder, proceed.
+    # Else delete this image.jpg (as it's already been copied over at an earlier date).
+    if (-not(Test-Path $landscapeCheck) -and -not(Test-Path $portraitCheck)) {  
+        # If the image dimensions are landscape, move it into the Landscape folder        
+        if ($h.Value[0] -gt $h.Value[1]) {
+            Move-Item $imageExt $landscapeFolder
+        }
+        # Elseif the image dimensions are portrait, move it into the Portrait folder
+        elseif ($h.Value[1] -gt $h.Value[0]) {
+            Move-Item $imageExt $portraitFolder
         }
     }
-    else { Remove-Item $image }
+    else { Remove-Item $imageExt }
 }
